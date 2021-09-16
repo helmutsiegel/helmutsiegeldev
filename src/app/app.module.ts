7a13ac11fc5c8f06382b03a80a2c99d0439db334
Helmut Siegel <helmutsiegel17@gmail.com>
2021-09-16 15:50:03 +0300

    Initial project

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExperiencesComponent,
+    EducationComponent,
+    SkillsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/experiences/experiences.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'experiences',
+  templateUrl: './experiences.component.html',
+  styleUrls: ['./experiences.component.scss']
+})
+export class ExperiencesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
