b84d426da3652fab1cf851cd01c6395d37628214
Helmut Siegel <helmutsiegel17@gmail.com>
2021-09-16 19:50:18 +0300

    CV

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ExperiencesComponent } from './experiences/experiences.component';
 import { EducationComponent } from './education/education.component';
 import { SkillsComponent } from './skills/skills.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExperiencesComponent,
     EducationComponent,
-    SkillsComponent
+    SkillsComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule
